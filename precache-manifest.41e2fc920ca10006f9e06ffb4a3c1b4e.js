8a4adc44468fac452a1d537e01cada8f2d36cf5f
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2020-01-02 23:19:58 +0530

    Updates

---
+++ precache-manifest.41e2fc920ca10006f9e06ffb4a3c1b4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d36ee33cb98998bf52531d30b0f6e0b",
+    "url": "/my-firt-react-project/index.html"
+  },
+  {
+    "revision": "18e9ba09db86d5ea399e",
+    "url": "/my-firt-react-project/static/css/main.0a7db816.chunk.css"
+  },
+  {
+    "revision": "434dcb551cfcd9aa664d",
+    "url": "/my-firt-react-project/static/js/2.42ae6044.chunk.js"
+  },
+  {
+    "revision": "18e9ba09db86d5ea399e",
+    "url": "/my-firt-react-project/static/js/main.2fa4eba6.chunk.js"
+  },
+  {
+    "revision": "dfccf929d245fa16a6c4",
+    "url": "/my-firt-react-project/static/js/runtime~main.85cc0fc3.js"
+  }
+]);
